500e0ca6d92282a9c83336f3dbab968ef3514ba1
Cho Won Jae <yoteamo7@naver.com>
2020-07-14 17:13:10 +0900

    react - state

--- src/App.js
+++ src/App.js
@@ -1,39 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-const foodILike= [
-  {
-    id : 1,
-    name : "Kimchi",
-    image : "https://images.unsplash.com/photo-1583224944844-5b268c057b72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" ,
-    rating : 5
-  },
-  {
-    id : 2,
-    name : "Samgyeopsal",
-    image : "https://images.unsplash.com/photo-1550388341-d3d5ac2a724e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-    rating : 4.9
-  },
-  { 
-    id : 3,
-    name : "Bibimbap",
-    image : "https://images.unsplash.com/photo-1553163147-622ab57be1c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-    rating : 4.8
-  },
-  {
-    id : 4,
-    name : "Doncasu",
-    image : "https://images.unsplash.com/photo-1513262599279-d287e25f4d84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-    rating : 4.7
-  },
-  {
-    id : 5,
-    name : "Kimbap",
-    image : "https://images.unsplash.com/photo-1532347231146-80afc9e3df2b?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    rating : 4.6
-  }
-];
-
 function Food({name, picture, rating}){
   return (
   <div>
@@ -50,19 +17,38 @@ Food.propTypes = {
   rating : PropTypes.number.isRequired
 }
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => 
-      <Food 
-        key = {dish.id}
-        name = {dish.name}
-        picture = {dish.image} 
-        rating = {dish.rating} 
-      />
-      )}
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
+  state = {
+    count : 0
+  };
+  add = () => {
+    this.setState(current => ({count : current.count +1}));
+  };
+  minus = () => {
+    this.setState(current => ({count : current.count -1}));
+  };
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just Update!");
+  }
+  componentWillUnmount(){
+    console.log("Goodbye, cruel world");
+  }
+  render(){
+    console.log("I`am rendering");
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick = {this.add}>Add</button>
+        <button onClick = {this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
 export default App;
